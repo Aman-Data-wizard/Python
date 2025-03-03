1f1994c3160ef0904b252665984d7b52eef069b3
Aman <singh.mie4166@gmail.com>
2025-03-03 19:55:17 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,10 @@
+
+s = 'Raman'
+print (s[:2])
+print (s[2:])
+this_list = ["Apple", "Banana", "Cherry"]
+for x in this_list:
+     print(x)
+if "Apple" in this_list:
+     print("ok")
+     
---
+++ splitting.py
@@ -0,0 +1,12 @@
+# str1= input("Enter a paragraph")
+# list = str1.split()
+# print(list)
+
+str1 = 'banana mango pine wine'
+list1 = str1.split()
+print(list1)
+
+str1 = 'Value1 Value2 Value3'
+a, b, c = str1.split()
+print(a, b, c)
+
